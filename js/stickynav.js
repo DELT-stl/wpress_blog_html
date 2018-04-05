73a8ea7258c58a78c1536e0611edb0939384ab39
stlDELT <josh@deltstl.com>
2018-04-05 11:23:32 -0500

    products page

--- js/stickynav.js
+++ js/stickynav.js
@@ -1,16 +1,17 @@
+jQuery(document).ready(function ($) {
 var bdh = $("body").height();
 var products = new Array($(".products").length);
 
 $('.main-nav-list').sticky({
     topSpacing: 410,
     zIndex: 100,
-    stopper: "#footer",
-    stickyClass: ".main-nav-scrolled"
+    stopper: "footer",
+    stickyClass: "main-nav-scrolled"
 });
     //EXECUTES ONLY ONCE
-function once(fn, context) { 
+function once(fn, context) {
 	var result;
-	return function() { 
+	return function() {
 		if(fn) {
 			result = fn.apply(context || this, arguments);
 			fn = null;
@@ -20,12 +21,19 @@ function once(fn, context) {
 }
 
 var links = $('.main-nav-list li');
+var p = $('.products');
 $(window).on('scroll', function() {
     $('.products').each(function(i, obj) {
-        if($(window).scrollTop() >= $(this).offset().top) {
+        if($(window).scrollTop() >= $(this).offset().top - $(this).height()) {
              var id = $(this).attr('id');
             $('li').removeClass('hi-lited');
             $(links.eq(i)).addClass('hi-lited');
            }
+        if ($(window).scrollTop()+$(window).height() <= $('.products').eq(1).offset().top) {
+            $('li').removeClass('hi-lited');
+        }
+
     });
+});
+
 });
